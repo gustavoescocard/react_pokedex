eafeb7cefe3d73cedd3cf85ee01d81ebba215344
Gustavo Escocard <gustavo.escocard@gmail.com>
2023-05-02 12:01:10 -0300

    Create pagination component

--- src/App.js
+++ src/App.js
@@ -7,12 +7,16 @@ import Pokedex from './components/Pokedex';
 import { getPokemonData, getPokemons } from './api';
 
 function App() {
+  const [page, setPage] = useState(0)
+  const [totalPages, setTotalPages] = useState(0)
   const [loading, setLoading] = useState(false)
   const [pokemons, setPokemons] = useState([])
+
+  const itensPerPage = 25
   const fetchPokemons = async () => {
     try {
       setLoading(true)
-      const data = await getPokemons()
+      const data = await getPokemons(itensPerPage, itensPerPage * page)
       const promises = data.results.map(async (pokemon) => {
         return await getPokemonData(pokemon.url)
       })
@@ -20,21 +24,26 @@ function App() {
       const results = await Promise.all(promises)
       setPokemons(results)
       setLoading(false)
+      setTotalPages(Math.ceil((data.count / itensPerPage)))
     } catch (error) {
       console.log("fetchPokemons error ", error)
     }
   }
 
   useEffect(() => {
-    console.log("carregou")
     fetchPokemons();
-  }, [])
+  }, [page])
 
   return (
     <>
       <Navbar />
       <Searchbar />
-      <Pokedex pokemons={pokemons} loading={loading}/>
+      <Pokedex
+        pokemons={pokemons}
+        loading={loading}
+        page={page}
+        setPage={setPage}
+        totalPages={totalPages} />
     </>
   );
 }
